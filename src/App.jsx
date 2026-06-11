3afd239630685a26a9ce91c01c9fc0c1d8153392
Pablo Moralez <pablo.moralez@aluno.senai.br>
2026-06-11 11:30:39 -0300

    FINAlIZEI O DE HOJE

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,7 @@ function App() {
 
   return (
     <>
-      <UseStateHook/>
-
+      <Contador/>
     </>
   )
 }
--- src/components/Contador/Contador.jsx
+++ src/components/Contador/Contador.jsx
@@ -3,6 +3,9 @@ import { useState } from "react"
 function Contador (){
     return(
         <h1>Aqui vai ser o contador</h1>
+        <button>ADD</button>
+        <button>REMOVER</button>
+        <button>REINICIAR</button>
     )
 }
 
