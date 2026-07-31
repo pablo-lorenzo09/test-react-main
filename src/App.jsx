b9c5572a83bc032e098c566c584f9087b2d46dac
Pablo Moralez <pablo.moralez@aluno.senai.br>
2026-07-31 08:59:26 -0300

    COMMIT: ADD TEMA

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Alunos from './components/Alunos/Alunos.jsx'
-import Card from './components/Card/Card.jsx'
-import Button from './components/Button/Button.jsx'
-import Lista from './components/Lista/Lista.jsx'
-import MsgUsuario from './components/MensagemUsuario/MsgUsuario.jsx'
-import UseStateHook from './components/UseStateHook/UseStakeHook.jsx'
-import UseEffectHook from './components/UseEffectHook/UseEffectHook.jsx'
-import Contador from './components/Contador/Contador.jsx'
+// import Alunos from './components/Alunos/Alunos.jsx'
+// import Card from './components/Card/Card.jsx'
+// import Button from './components/Button/Button.jsx'
+// import Lista from './components/Lista/Lista.jsx'
+// import MsgUsuario from './components/MensagemUsuario/MsgUsuario.jsx'
+// import UseStateHook from './components/UseStateHook/UseStakeHook.jsx'
+// import UseEffectHook from './components/UseEffectHook/UseEffectHook.jsx'
+// import Contador from './components/Contador/Contador.jsx'
 import Tema from './components/Tema/Tema.jsx'
 
 import './App.css'
@@ -18,6 +18,7 @@ function App() {
   return (
     <>
       <Tema/>
+      {/* <ContadorAuto/> */}
     </>
   )
 }
--- src/components/Tema/Tema.jsx
+++ src/components/Tema/Tema.jsx
@@ -7,58 +7,81 @@ function Tema() {
     const [contador, setContador] = useState(0);
 
     // Caso 2: texto digitado pelo usuário
-    const [nome, setNome] = useState("");
+    const [nome, setNome] = useState("visitante");
     // Caso 3: controle de tema
     const [temaEscuro, setTemaEscuro] = useState(false)
     // Casos de uso do useEffect
 
     // Caso 1: Executa apenas uma vez, ao carregar o componente
     useEffect(() => {
-
+        console.log("Componente carregado pela primeira vez")
     }, []);
     // Caso 2: executa sempre que o contador mudar
     useEffect(() => {
-
+        console.log("O contador foi alterado para", contador);
     }, [contador]);
     // Caso 3: executa sempre que o tema mudar
     useEffect(() => {
-
+        console.log("Tema alterado:", temaEscuro ? "Escuro" : "Claro")
     }, [temaEscuro]);
     
     // função pra aumentar contador
     function aumentarContador(){
         setContador(contador + 1)
     }
-    // função pra aumentar contador
+    // função pra diminuir contador
     function diminuirContador(){
         setContador(contador - 1)
     }
-    // função pra aumentar contador
+    // função pra alterar tema
     function alternarTema(){
-        
+        setTemaEscuro(!temaEscuro)
     }
 
     return(
         <>
            <main
-           className={`${styles.container
-    } ${temaEscuro ? styles.temaEscuro : styles.temaClaro}`}>
+           className={`${
+            styles.container} ${temaEscuro ? styles.temaEscuro : styles.temaClaro}`}>
 
         <h1 className={styles.titulo}>Exemplos de useState e useEffect</h1>
 
         {/* Caso 1: Contador */}
         <section className={styles.card}>
+            <h1>Contador</h1>
+            <p className={styles.valor}>0</p>
+            <div className={styles.botoes}>
+                <button>+</button>
+                <button>-</button>
+            </div>
+            
+            <p>O useEffect observa a variável <strong>contador</strong> e executa sempre que ela muda.</p>
+
         </section>
 
         {/* Caso 2: Campo de texto */}
         <section className={styles.card}>
+            <h1>Campo de texto (useState)</h1>
+            <input 
+            className={styles.input} 
+            type="text" 
+            name="" 
+            id="input_nome" 
+            placeholder='Digite seu nome' 
+            onInput={() => {setNome(document.getElementById("input_nome").value)}}/>
+            <p>Olá, <strong>{nome}!</strong></p>
         </section>
 
         {/* Caso 3: Alternância de tema */}
         <section className={styles.card}>
+            <h1>3. Alternância de tema (useState)</h1>
+            <div className={styles.botoes}>
+                <button onClick={alternarTema}>Alternar para tema {temaEscuro ? "Claro" : "Escuro"}</button>
+            </div>
+            <p>o useEffect observa a variável <strong>temaEscuro</strong> e executa sempre que o tema é alterado.</p>
         </section>
 
-
+            
            </main> 
         </>
     );
