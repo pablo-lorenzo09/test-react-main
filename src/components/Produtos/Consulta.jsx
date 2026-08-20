6d987b08aa0c042d979c2c938b39fac4a9a43e38
Pablo Moralez <pablo.moralez@aluno.senai.br>
2026-08-20 11:25:27 -0300

    oi

--- src/components/Produtos/Consulta.jsx
+++ src/components/Produtos/Consulta.jsx
@@ -2,27 +2,34 @@ import { useState, useEffect } from "react";
 import Produto from "./Produto";
 
 function Consulta() {
-
+// Estado que vai controlar a renderização do produto na UI
   const [produto, setProduto] = useState(null);
-
+// Inserir o produto no Local Storage
   useEffect(() => {
-    
+  const produtoLocal = localStorage.getItem('produto');
+  if (produtoLocal !== 'null') setProduto(produtoLocal)
   }, []);
-
+// Recuperar o produto do Local Storage quando o produto for modificado pelo estado
   useEffect(() => {
-    
+    if (produto !== null) localStorage.setItem('produto', produto);
   }, [produto]);
-
+// Extrair o texto do Botão quando o usuário clicar
   function handleClick({ target }) {
+    setProduto(target.innerText);
   }
 
-  return(
-      <>    
-          <h1>Preferência:</h1>
-          <button>notebook</button> 
-          <button>smartphone</button>
-      </>
-  )
+  return (
+    <section>
+      <h1>Preferência:{produto}</h1>
+      <button onClick={handleClick}>
+        notebook
+      </button>
+      <button onClick={handleClick}>
+        smartphone
+      </button>
+      <Produto produto={produto}/>
+    </section>
+  );
 }
 
-export default Consulta
+export default Consulta;
